3efe63370db6796fc63aa458a9df120b95fc85d7
Geoffrey BErgen <bergen.geoff@gmail.com>
2026-05-13 14:43:50 +0000

    pathing

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
-import { useNavigate, Link } from 'react-router-dom';
 import { supabase } from '../supabase';
 import StoryModal from '../components/StoryModal';
 import TheWell from '../utils/TheWell';
